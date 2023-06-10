88fcabe4d62a857a13ca2ec988208be98a1bd351
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-10 22:24:12 +0300

    boş gönderme engellendi ve styled-component ile button oluşturuldu.

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
 import "./TodoForm.css";
-import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const TodoForm = (props) => {
   const [enteredTodo, setEnteredTodo] = useState("");
+  const [isValid, setIsValid] = useState(true);
   const todoChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
+
     setEnteredTodo(event.target.value);
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    if (enteredTodo.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
 
     const formData = { text: enteredTodo };
     props.onAddTodo(formData);
@@ -21,9 +30,10 @@ const TodoForm = (props) => {
   return (
     <div className="todoForm">
       <form onSubmit={submitHandler}>
-        <div className="form-control">
+        <div className={`form-control ${!isValid && "invalid"}`}>
           <label>Todo</label>
           <input type="text" value={enteredTodo} onChange={todoChangeHandler} />
+          {!isValid && <span>Please enter a valid todo!</span>}
         </div>
         <Button type="submit">Add Todo</Button>
       </form>
