4edcafe45beab91ec13fda914209cd6c6ea3f7b4
Ahmet Sarıkaya <ahmetsarikaya0696@outlook.com>
2023-06-11 13:17:49 +0300

    CSS Module kullanarak TodoForm ve Button update edildi.

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,39 +1,41 @@
 import React, { useState } from "react";
-import "./TodoForm.css";
+// import "./TodoForm.css";
+import styles from "./TodoForm.module.css";
 import Button from "../UI/Button";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// Styled-Components Approach
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => props.invalid && "red"};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => props.invalid && "red"};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-    border-radius: 6px;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//     border-radius: 6px;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-  & span {
-    color: red;
-    font-size: smaller;
-  }
-`;
+//   & span {
+//     color: red;
+//     font-size: smaller;
+//   }
+// `;
 
 const TodoForm = (props) => {
   const [enteredTodo, setEnteredTodo] = useState("");
@@ -60,14 +62,45 @@ const TodoForm = (props) => {
     setEnteredTodo("");
   };
 
+  // CSS Approach
+  // return (
+  //   <div className="todoForm">
+  //     <form onSubmit={submitHandler}>
+  //       <div className={`form-control ${!isValid && "invalid"}`}>
+  //         <label>Todo</label>
+  //         <input type="text" value={enteredTodo} onChange={todoChangeHandler} />
+  //         {!isValid && <span>Please enter a valid todo!</span>}
+  //       </div>
+  //       <Button type="submit">Add Todo</Button>
+  //     </form>
+  //   </div>
+  // );
+
+  // styledComponent Approach
+  // return (
+  //   <div className="todoForm">
+  //     <form onSubmit={submitHandler}>
+  //       <FormControl invalid={!isValid}>
+  //         <label>Todo</label>
+  //         <input type="text" value={enteredTodo} onChange={todoChangeHandler} />
+  //         {!isValid && <span>Please enter a valid todo!</span>}
+  //       </FormControl>
+  //       <Button type="submit">Add Todo</Button>
+  //     </form>
+  //   </div>
+  // );
+
+  // CSS Module Approach
   return (
-    <div className="todoForm">
+    <div className={styles.todoForm}>
       <form onSubmit={submitHandler}>
-        <FormControl invalid={!isValid}>
+        <div
+          className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+        >
           <label>Todo</label>
           <input type="text" value={enteredTodo} onChange={todoChangeHandler} />
           {!isValid && <span>Please enter a valid todo!</span>}
-        </FormControl>
+        </div>
         <Button type="submit">Add Todo</Button>
       </form>
     </div>
